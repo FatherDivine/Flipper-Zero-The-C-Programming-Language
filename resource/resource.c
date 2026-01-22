c6acd4baa4a29310e44bd902805a4a2da05a8135
Father Divine <150489354+FatherDivine@users.noreply.github.com>
2026-01-21 22:21:27 -0700

    Ch 3 and appendixes addition (#14)

--- resource/resource.c
+++ resource/resource.c
@@ -1,5 +1,6 @@
 #include "resource.h"
 
+// Topics Section
 Topic appendixa[] = {
     {"A1. Introduction", NULL, APP_ASSETS_PATH("appendixa/0-a1.txt")}, 
     {"A2. Lexical Conventions", NULL, APP_ASSETS_PATH("appendixa/1-a2.txt")}, 
@@ -12,12 +13,27 @@ Topic appendixa[] = {
     {"A9. Statements", NULL, APP_ASSETS_PATH("appendixa/8-a9.txt")}, 
     {"A10. External Declarations", NULL, APP_ASSETS_PATH("appendixa/9-a10.txt")}, 
     {"A11. Scope and Linkage", NULL, APP_ASSETS_PATH("appendixa/10-a11.txt")}, 
-    {"A12. Preprocessing", NULL, APP_ASSETS_PATH("appendixa/11-a12.txt")}, 
+    {"A12. Preprocessing", NULL, APP_ASSETS_PATH("appendixa/11-a12.txt")},
+    {"A13. Grammar", NULL, APP_ASSETS_PATH("appendixa/12-a13.txt")},
 };
 
-Topic preface[] = {
-    {"Preface", NULL, APP_ASSETS_PATH("preface/preface.txt")},
-    {"Preface to the first edition", NULL, APP_ASSETS_PATH("preface/preface_to_the_first_edition.txt")}
+Topic appendixb[] = {
+    {"B0. Appendix B: Standard Library", NULL, APP_ASSETS_PATH("appendixb/0-b0.txt")}, 
+    {"B1. Input and Output: <stdio.h>", NULL, APP_ASSETS_PATH("appendixb/1-b1.txt")}, 
+    {"B2. Character Class Tests: <ctype.h>", NULL, APP_ASSETS_PATH("appendixb/2-b2.txt")}, 
+    {"B3. String Functions: <string.h>", NULL, APP_ASSETS_PATH("appendixa/3-b3.txt")}, 
+    {"B4. Mathematical Functions: <math.h>", NULL, APP_ASSETS_PATH("appendixb/4-b4.txt")}, 
+    {"B5. Utility Functions: <stdlib.h>", NULL, APP_ASSETS_PATH("appendixb/5-b5.txt")}, 
+    {"B6. Diagnostics: <assert.h>", NULL, APP_ASSETS_PATH("appendixb/6-b6.txt")}, 
+    {"B7. Variable Argument Lists: <stdarg.h>", NULL, APP_ASSETS_PATH("appendixb/7-b7.txt")}, 
+    {"B8. Non-local Jumps: <setjmp.h>", NULL, APP_ASSETS_PATH("appendixb/8-b8.txt")}, 
+    {"B9. Signals: <signal.h>", NULL, APP_ASSETS_PATH("appendixb/9-b9.txt")}, 
+    {"B10. Date and Time Functions: <time.h>", NULL, APP_ASSETS_PATH("appendixb/10-b10.txt")}, 
+    {"B11. Implementation-defined Limits: <limits.h> and <float.h>", NULL, APP_ASSETS_PATH("appendixb/11-b11.txt")}, 
+};
+
+Topic appendixc[] = {
+    {"Summary of Changes", NULL, APP_ASSETS_PATH("appendixc/0-summaryofchanges.txt")}, 
 };
 
 Topic chapter1[] = { 
@@ -52,8 +68,17 @@ Topic chapter2[] = {
     {"Exercises", NULL, APP_ASSETS_PATH("chapter2/exercises.txt")},
 };
 
-// Topic chapter3[] = {
-// };
+Topic chapter3[] = {
+    {"3.0 Control Flow", NULL, APP_ASSETS_PATH("chapter3/0-chapter3.txt")},
+    {"3.1 Statements and Blocks", NULL, APP_ASSETS_PATH("chapter3/1-chapter3.1.txt")},
+    {"3.2 If-Else", NULL, APP_ASSETS_PATH("chapter3/2-chapter3.2.txt")},
+    {"3.3 Else-If", NULL, APP_ASSETS_PATH("chapter3/3-chapter3.3.txt")},
+    {"3.4 Switch", NULL, APP_ASSETS_PATH("chapter3/4-chapter3.4.txt")},
+    {"3.5 Loops—While and For", NULL, APP_ASSETS_PATH("chapter3/5-chapter3.5.txt")},
+    {"3.6 Loops—Do-while", NULL, APP_ASSETS_PATH("chapter3/6-chapter3.6.txt")},
+    {"3.7 Break and Continue", NULL, APP_ASSETS_PATH("chapter3/7-chapter3.7.txt")},
+    {"3.8 Goto and Labels", NULL, APP_ASSETS_PATH("chapter3/8-chapter3.8.txt")},
+};
 
 // Topic chapter4[] = {
 // };
@@ -70,21 +95,33 @@ Topic chapter2[] = {
 // Topic chapter8[] = {
 // };
 
+Topic index[] = {
+    {"Index", NULL, APP_ASSETS_PATH("index/0-index.txt")}, 
+};
 Topic license[] = {
     {"LICENSE", NULL, APP_ASSETS_PATH("LICENSE.txt")},
 };
 
+Topic preface[] = {
+    {"Preface", NULL, APP_ASSETS_PATH("preface/preface.txt")},
+    {"Preface to the first edition", NULL, APP_ASSETS_PATH("preface/preface_to_the_first_edition.txt")}
+};
+
+// Chapters Section
 Chapter chapters[] = {
     {"Preface", preface, sizeof(preface) / sizeof(Topic)},
     {"Ch1-Introduction", chapter1, sizeof(chapter1) / sizeof(Topic)},
     {"Ch2-Types, Ops, Expr", chapter2, sizeof(chapter2) / sizeof(Topic)},
-    // {"Ch3-Control Flow", chapter3, sizeof(chapter3) / sizeof(Topic)},
+    {"Ch3-Control Flow", chapter3, sizeof(chapter3) / sizeof(Topic)},
     // {"Ch4-Functions", chapter4, sizeof(chapter4) / sizeof(Topic)},
     // {"Ch5-Pointers, Arrays", chapter5, sizeof(chapter5) / sizeof(Topic)},
     // {"Ch6-Structures", chapter6, sizeof(chapter6) / sizeof(Topic)},
     // {"Ch7-Input, Output", chapter7, sizeof(chapter7) / sizeof(Topic)},
     // {"Ch8-UNIX Sys Interface", chapter8, sizeof(chapter8) / sizeof(Topic)},
     {"Appendix A", appendixa, sizeof(appendixa) / sizeof(Topic)},
+    {"Appendix B", appendixb, sizeof(appendixb) / sizeof(Topic)},
+    {"Appendix C", appendixc, sizeof(appendixc) / sizeof(Topic)},
+    {"Index", index, sizeof(index) / sizeof(Topic)},
     {"LICENSE", license, sizeof(license) / sizeof(Topic)},
 };
 
